9f6cea4cf5cb130124c0cf6d301f3c31b664f683
dbfgdbdfg <2464012@ewha.ac.kr>
2024-07-19 16:57:56 +0900

    Title: 비오는 날, Time: 126ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240719/비오는 날/rainy-day.py
+++ 240719/비오는 날/rainy-day.py
@@ -1,7 +1,19 @@
 n=int(input())
+a_list=[]
 for i in range(n):
     a=input()
     w=a.split()
     if w[2]=="Rain":
-        print(a)
-        break
+        a_list.append(a)
+for i in a_list:
+    for q in a_list:
+        if int(i[:3])<int(q[:3]):
+            print(i)
+        if int(i[:3])==int(q[:3]):
+            if int(i[5:7])<int(q[5:7]):
+                print(i)
+
+            if int(i[5:7])==int(q[5:7]):
+
+                if int(i[8:10])<int(q[8:10]):
+                    print(i)
