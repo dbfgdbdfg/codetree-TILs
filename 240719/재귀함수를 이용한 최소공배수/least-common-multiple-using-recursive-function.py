7f9c773524883a6360d8e25ab47bf78bf1561611
dbfgdbdfg <2464012@ewha.ac.kr>
2024-07-19 15:54:55 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240719/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
+++ 240719/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
@@ -1,20 +1,43 @@
 n=int(input())
 w=input().split()
 a_list=[]
-nums=1
 for i in range(n):
     a=int(w[i])
     a_list.append(a)
-    nums*=a
+
 b_list=[]
-for i in range(1,max(a_list)):
+for i in a_list:
+    nums=0
+    for q in range(2,i):        
+        if i%q==0:
+            b_list.append(q)
+            nums+=1
+        if nums==0:
+            b_list.append(i)
+
+b__list=list(set(b_list))
+
+for a in b__list:
+    for w in b__list:
+        if w!=a and w%a==0:
+            b__list.remove(a)
+final=1
+for i in b__list:
+    final*=i
+print(final)
+
+'''
+b_list=[]
+c_list=[]
+
+for i in range(1,10):
     t=nums/i
     final=t
-    for q in a_list:
-        
-        if t%q!=0:
-            final=0   
-    if final!=0:
+    for q in a_list:        
+        c_list.append(t%q)
+        c__list=list(set(c_list))
+    if c__list==[0.0]:
         b_list.append(final)
-        
-print(int(min(b_list)))
+print(b_list)       
+#print(int(min(b_list)))
+'''
