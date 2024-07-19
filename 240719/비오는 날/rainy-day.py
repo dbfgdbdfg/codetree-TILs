e3145f5459eb2bf79e27bd25df8fc84d169e21fd
dbfgdbdfg <2464012@ewha.ac.kr>
2024-07-19 17:11:12 +0900

    Title: 비오는 날, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 240719/비오는 날/rainy-day.py
+++ 240719/비오는 날/rainy-day.py
@@ -5,15 +5,14 @@ for i in range(n):
     w=a.split()
     if w[2]=="Rain":
         a_list.append(a)
+
+#['2037-12-27 Sun Rain', '2036-08-28 Wed Rain', '2043-03-21 Sat Rain', '2077-08-19 Thu Rain']
+b_list=[]
 for i in a_list:
-    for q in a_list:
-        if int(i[:3])<int(q[:3]):
-            print(i)
-        if int(i[:3])==int(q[:3]):
-            if int(i[5:7])<int(q[5:7]):
-                print(i)
+    ai=i[:4]+i[5:7]+i[8:10]
+    b_list.append(int(ai))
+what = b_list.index(min(b_list))
 
-            if int(i[5:7])==int(q[5:7]):
+    #for q in a_list:
 
-                if int(i[8:10])<int(q[8:10]):
-                    print(i)
+print(a_list[what])
