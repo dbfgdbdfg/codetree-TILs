79aed307f48e2f186b0149c5c91724dc86bcd250
dbfgdbdfg <2464012@ewha.ac.kr>
2024-07-26 17:32:19 +0900

    Title: 요일 맞추기, Time: 147ms, Memory: 24MB, Status: Passed - CodeTree

--- 240726/요일 맞추기/guess-day-of-week.py
+++ 240726/요일 맞추기/guess-day-of-week.py
@@ -4,10 +4,16 @@ w=input().split()
 m1,d1,m2,d2=int(w[0]),int(w[1]),int(w[2]),int(w[3])
 
 date=0
-for i in range(m1,m2):
-    date=date+month[i]
-date = date +d2-d1 +7
+if m1<=m2:
+    for i in range(m1,m2):
+        date=date+month[i]
+    date = date +d2-d1 +7
+if m1>m2:
+    for i in range(m2,m1):
+        date=date-month[i]
+    date = date -d1+d2
 
 
 i=(date%7 + 1)%7
+
 print(days[i])
