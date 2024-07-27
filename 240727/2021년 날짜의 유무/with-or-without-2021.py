fb288cbefa637678577f7a2c1a45c32fcc966eb8
dbfgdbdfg <2464012@ewha.ac.kr>
2024-07-27 17:53:49 +0900

    Title: 2021년 날짜의 유무, Time: 197ms, Memory: 24MB, Status: Passed - CodeTree

--- 240727/2021년 날짜의 유무/with-or-without-2021.py
+++ 240727/2021년 날짜의 유무/with-or-without-2021.py
@@ -3,7 +3,12 @@ m,d=int(w[0]),int(w[1])
 a_list=[0,31,28,31,30,31,30,31,31,30,31,30,31]
 
 def right(a,b):
-    return int(a_list[a])>=b
+    if a>12:
+        return False
+    if a<13 and int(a_list[a])<b:
+        return False
+    return True
+
 if right(m,d):
     print("Yes")
 else:
