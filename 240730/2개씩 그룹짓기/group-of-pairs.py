6679485b00b6148cd776b44121a566f183ea2f34
dbfgdbdfg <2464012@ewha.ac.kr>
2024-07-30 14:17:02 +0900

    Title: 2개씩 그룹짓기, Time: 150ms, Memory: 25MB, Status: Passed - CodeTree

--- 240730/2개씩 그룹짓기/group-of-pairs.py
+++ 240730/2개씩 그룹짓기/group-of-pairs.py
@@ -2,4 +2,9 @@ n=int(input())
 
 a_list=list(map(int,input().split()))
 a_list.sort()
-print(a_list[n**2-1]+a_list[0])
+
+b_list=[]
+for i in range(n*2):
+    b_list.append(a_list[i]+a_list[n*2-1-i])
+
+print(max(b_list))
