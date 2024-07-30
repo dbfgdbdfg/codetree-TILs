9aa9f6ebcec6937a74557bc203248c012608e7e1
dbfgdbdfg <2464012@ewha.ac.kr>
2024-07-30 14:04:30 +0900

    Title: 단어 정렬, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240730/단어 정렬/sorting-words.py
@@ -0,0 +1,15 @@
+n=int(input())
+a_list=[]
+for i in range(n):
+    a_list.append(input())
+a_list.sort()
+for i in range(n):
+    print(a_list[i])
+
+
+
+'''
+words = ["banana", "apple", "cat", "app"]
+words.sort()
+print(words) # ['app', 'apple', 'banana', 'cat']
+'''
