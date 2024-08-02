22145f6561a326f5965d84c708d72ca404194dcf
dbfgdbdfg <2464012@ewha.ac.kr>
2024-08-02 17:22:37 +0900

    Title: 줄 세우기, Time: 149ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240802/줄 세우기/line-up-students.py
@@ -0,0 +1,16 @@
+n=int(input())
+
+class Great:
+    def __init__(self,key,wei,num):
+        self.key=key
+        self.wei=wei
+        self.num=num
+a_list=[]
+for i in range(1,n+1):
+    w=input().split()
+    a_list.append(Great(int(w[0]),int(w[1]),i))
+
+a_list.sort(key=lambda a:(-a.key,-a.wei,-a.num))
+
+for Great in a_list:
+    print(Great.key,Great.wei,Great.num)
