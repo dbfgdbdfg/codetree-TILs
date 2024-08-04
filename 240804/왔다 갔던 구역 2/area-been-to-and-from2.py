8cffaf3beaef722720b74806ba5684cd9ab5c415
dbfgdbdfg <2464012@ewha.ac.kr>
2024-08-04 16:18:11 +0900

    Title: 왔다 갔던 구역 2, Time: 107ms, Memory: 25MB, Status: Passed - CodeTree

--- 240804/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 240804/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -1,25 +1,30 @@
-n=int(input())
-a_list=[]
+n = int(input())
+a_list = []
+
 first=0
-final=0
+
 for i in range(n):
     w=input().split()
     a,b=int(w[0]),w[1]
     if b=="R":
-        for i in range(0,a):
-            a_list.append(first+i)
+        end=first+a
+        a_list.append([first,end])
         first+=a
     if b=="L":
-        for i in range(0,a):
-            a_list.append(first-i)
+        end=first-a
+        a_list.append([end,first])
         first-=a
+	
+b_list = [0] *2000
 
-b_list=set(a_list)
-#print(a_list)
-#print(b_list)
-for i in b_list:
-    t=a_list.count(i)
-    if t>=2:
-        final+=1
-    
-print(final)
+for x,y in a_list:
+    for i in range(x+1000,y+1000):
+        b_list[i]+=1
+
+
+cnt = 0
+
+for elem in b_list:
+	if elem >= 2:
+		cnt += 1
+print(cnt)
