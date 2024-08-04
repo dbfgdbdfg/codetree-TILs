08f6385374fea4dd4c8a999acf4acd333f1068d7
dbfgdbdfg <2464012@ewha.ac.kr>
2024-08-04 17:11:12 +0900

    Title: 흰검 칠하기, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240804/흰검 칠하기/painting-white-black.py
@@ -0,0 +1,29 @@
+n=int(input())
+#www bbb fff
+first=1000
+a_list=[0]*2000
+w_list=[]
+b_list=[]
+
+
+for i in range(n):
+    w=input().split()
+    a,b=int(w[0]),w[1]
+    if b=="R":
+        for t in range(a):
+            a_list[first+t]="W"
+            w_list.append(first+t)
+        first+=a
+
+    if b=="L":
+        for t in range(a):
+            a_list[first-t-1]="B"
+            b_list.append(first-t-1)
+        first-=a 
+
+
+for i in w_list:
+    if w_list.count(i)>=2 and b_list.count(i)>=2:
+        a_list[i]="F"
+
+print(a_list.count("B"),a_list.count("W"),a_list.count("F"))
