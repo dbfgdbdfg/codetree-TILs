e93f29a205c26a5e6021966625db6df1800be3e7
dbfgdbdfg <2464012@ewha.ac.kr>
2024-08-04 15:51:29 +0900

    Title: 왔다 갔던 구역 2, Time: 122ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240804/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 240804/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -1,17 +1,25 @@
 n=int(input())
 a_list=[]
 first=0
+final=0
 for i in range(n):
     w=input().split()
     a,b=int(w[0]),w[1]
     if b=="R":
-        for i in range(1,a+1):
+        for i in range(0,a):
             a_list.append(first+i)
         first+=a
     if b=="L":
-        for i in range(1,a+1):
+        for i in range(0,a):
             a_list.append(first-i)
         first-=a
 
 b_list=set(a_list)
-print(len(a_list)-len(b_list))
+#print(a_list)
+#print(b_list)
+for i in b_list:
+    t=a_list.count(i)
+    if t>=2:
+        final+=1
+    
+print(final)
