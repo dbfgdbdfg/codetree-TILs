33f95ee6fa44c8e76237d448803118badd24246d
dbfgdbdfg <2464012@ewha.ac.kr>
2024-08-04 19:14:03 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 151ms, Memory: 55MB, Status: Wrong Answer - CodeTree

--- 240804/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
+++ 240804/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -1,43 +1,19 @@
 a_list=[[0]*2001 for row in range(2001)]
-
+nums=0
 x1,y1,x2,y2=map(int,input().split())
-
 for r in range(x1+1000,x2+1000):
     for c in range(y1+1000,y2+1000):
         a_list[r][c]=1
 
 x11,y11,x22,y22=map(int,input().split())
-
 for r in range(x11+1000,x22+1000):
     for c in range(y11+1000,y22+1000):
         a_list[r][c]=0
 
-
-if x1>=x11 and y1>=y11 and x2<=x22 and y2<=y22:
-    print("0")
-
+if (y2>y22 and x1<x11) or (x22<x2 and y11>y1) or x2<=x11 or y2<=y11 or x1>x22 or y1>y22:
+    print((x2-x1)*(y2-y1))
 else:
-    while True:
-        a=x1
-        b=y1
-        if a_list[a+1000][b+1000]==1:
-            break
-    
-        a+=1
-        if a_list[a+1000][b+1000]==1:
-            break
-        a-=1
-        b+=1
-    
-
-    while True:
-        c=x2
-        d=y2
-        if a_list[c+1000][d+1000]==1:
-            break
-        c-=1
-        if a_list[c+1000][d+1000]==1:
-            break
-        c+=1
-        d-=1    
-    print((c-a+1)*(d-b+1))
+    for row in a_list:
+        for element in row:
+            nums+=element
+    print(nums)
