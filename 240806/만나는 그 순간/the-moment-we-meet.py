139d18dcb333547fe3203ed136e9a3a4363461e5
dbfgdbdfg <2464012@ewha.ac.kr>
2024-08-06 15:09:14 +0900

    Title: 만나는 그 순간, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240806/만나는 그 순간/the-moment-we-meet.py
@@ -0,0 +1,41 @@
+n,m=map(int,input().split())
+a_list=[]
+b_list=[]
+start=0
+sums=0
+
+for i in range(n):
+    w=input().split()
+    x,y=w[0],int(w[1])
+    sums+=y
+    if x=="R":
+        for q in range(1,y+1):
+            a_list.append(start+q)
+        start+=y
+    if x=="L":
+        for q in range(1,y+1):
+            a_list.append(start-q)
+        start-=y
+start=0
+for i in range(m):
+    w=input().split()
+    x,y=w[0],int(w[1])
+    if x=="R":
+        for q in range(1,y+1):
+            b_list.append(start+q)
+        start+=y
+    if x=="L":
+        for q in range(1,y+1):
+            b_list.append(start-q)
+        start-=y
+
+
+
+time=0
+while time<=sums:
+    if a_list[time]==b_list[time]:
+        print(time+1)
+        break
+    if time==sums:
+        print("-1")
+    time+=1
