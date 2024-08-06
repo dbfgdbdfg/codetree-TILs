6900388a3fc0e8e6af5d0ba3e5fa35588f99a458
dbfgdbdfg <2464012@ewha.ac.kr>
2024-08-06 14:42:13 +0900

    Title: 연속되는 수 3, Time: 127ms, Memory: 25MB, Status: Passed - CodeTree

--- 240806/연속되는 수 3/continuous-number3.py
+++ 240806/연속되는 수 3/continuous-number3.py
@@ -4,11 +4,17 @@ for i in range(n):
     a_list.append(int(input()))
 nums=1
 b_list=[]
-for i in range(n-1):
-    if a_list[i]*a_list[i-1]>0:
-        nums+=1
-        b_list.append(nums)
-    else:
-        nums=1
-        b_list.append(nums)
-print(max(b_list))
+
+if n==1:
+    print("1")
+else:
+
+
+    for i in range(n-1):
+        if a_list[i]*a_list[i-1]>0:
+            nums+=1
+            b_list.append(nums)
+        else:
+            nums=1
+            b_list.append(nums)
+    print(max(b_list))
