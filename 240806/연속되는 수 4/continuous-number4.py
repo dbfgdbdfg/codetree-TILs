52f9033dde726a9a7819f0c0b4d28b37fd66632f
dbfgdbdfg <2464012@ewha.ac.kr>
2024-08-06 14:44:51 +0900

    Title: 연속되는 수 4, Time: 120ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240806/연속되는 수 4/continuous-number4.py
@@ -0,0 +1,20 @@
+n=int(input())
+a_list=[]
+for i in range(n):
+    a_list.append(int(input()))
+nums=0
+b_list=[]
+
+
+if n==1:
+    print("1")
+else:
+    for i in range(n-1):
+        if a_list[i]<a_list[i+1]:
+            nums+=1
+            b_list.append(nums)
+        else:
+            nums=1
+            b_list.append(nums)
+
+    print(max(b_list))
