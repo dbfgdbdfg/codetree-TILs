abd766a975d900125197471ad4376f66289ba88d
dbfgdbdfg <2464012@ewha.ac.kr>
2024-08-06 14:40:52 +0900

    Title: 연속되는 수 3, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240806/연속되는 수 3/continuous-number3.py
@@ -0,0 +1,14 @@
+n=int(input())
+a_list=[]
+for i in range(n):
+    a_list.append(int(input()))
+nums=1
+b_list=[]
+for i in range(n-1):
+    if a_list[i]*a_list[i-1]>0:
+        nums+=1
+        b_list.append(nums)
+    else:
+        nums=1
+        b_list.append(nums)
+print(max(b_list))
