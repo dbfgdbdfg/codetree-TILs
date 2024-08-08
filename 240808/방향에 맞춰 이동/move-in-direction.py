be9648d51ed0839c5b07875e3f1503057b21a98c
dbfgdbdfg <2464012@ewha.ac.kr>
2024-08-08 15:04:50 +0900

    Title: 방향에 맞춰 이동, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240808/방향에 맞춰 이동/move-in-direction.py
+++ 240808/방향에 맞춰 이동/move-in-direction.py
@@ -2,17 +2,18 @@ n=int(input())
 x=0
 y=0
 dx, dy = [1, 0, -1, 0], [0, -1, 0, 1]
-
+c=0
 for i in range(n):
     w=input().split()
     a,b=w[0],int(w[1])
-    c=0
     if a=="N":
         c=3
     elif a=="S":
         c=1
-    elif a=="w":
+    elif a=="W":
         c=2
+    elif a=="E":
+        c=0
     
     x=x+dx[c]*b
     y=y+dy[c]*b
