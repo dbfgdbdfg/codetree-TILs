cd25568242b5511a9c275d2c4cf9e9b30a4f956d
dbfgdbdfg <2464012@ewha.ac.kr>
2024-08-10 15:57:12 +0900

    Title: 거울에 레이저 쏘기 2, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240810/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
+++ 240810/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
@@ -4,10 +4,9 @@ a_list=[list(input())
     for i in range(n)]
 # / 는 하나로 \는 \\로 입력됬다
 
-global dire
 dire=0
 #아래로 0 오른쪽 1 위로 2 왼쪽 3
-dx,dy=[-1,0,1,0],[0,1,0,-1]
+dx,dy=[1,0,-1,0],[0,1,0,-1]
 
 m=int(input())
 if m//n==0:
@@ -27,45 +26,46 @@ elif m//n==3:
     y=0
     dire =1
 #m을 이용해서 시작 좌표 생성
-#print(a_list)
-#print(x,y,dire)
+
+print(x,y,dire)
 
 ting=0
 
 def Great(n):
     if n==3:
-        n=2
+        return 2
     if n==2:
-        n=3
+        return 3
     if n==1:
-        n=0
+        return 0
     if n==0:
-        n=1
+        return 1
 def Fant(n):
     if n==0:
-        n=3
+        return 3
     if n==3:
-        n=0
+        return 0
     if n==1:
-        n=2
+        return 2
     if n==2:
-        n=1
+        return 1
 #아래로 0 오른쪽 1 위로 2 왼쪽 3
 
 
 
 while True:
     if a_list[x][y]=='\\':
-        Great(dire)
+        dire=Great(dire)
         ting+=1
         x=x+dx[dire]
         y=y+dy[dire]
-
-    if a_list[x][y]=="/":
-        Fant(dire)
+    
+    elif a_list[x][y]=="/":
+        dire=Fant(dire)
         ting+=1
         x=x+dx[dire]
         y=y+dy[dire]
+   
     if x<0 or y<0 or x>=n or y>=n:
         print(ting)
         break
