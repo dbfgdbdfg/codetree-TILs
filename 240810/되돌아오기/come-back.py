a5a7e8bb3c8a294274da33a24d6a7cd853853fb5
dbfgdbdfg <2464012@ewha.ac.kr>
2024-08-10 14:59:25 +0900

    Title: 되돌아오기, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240810/되돌아오기/come-back.py
+++ 240810/되돌아오기/come-back.py
@@ -3,48 +3,51 @@ x,y=0,0
 dx, dy = [1, 0, -1, 0], [0, -1, 0, 1]
 time=0
 c=0
-while True:
-    for i in range(n):
-        w=input().split()
-        a,b=w[0],int(w[1])
-        if a=="N":
-            c=3
-            for i in range(b):
-                time+=1
-                x=x+dx[c]
-                y=y+dy[c]
-                if x==0 and y==0:
-                    print(time)
-                    break
-        if a=="S":
-            c=1
-            for i in range(b):
-                time+=1
-                x=x+dx[c]
-                y=y+dy[c]
-                if x==0 and y==0:
-                    print(time)
-                    break
-   
-        if a=="W":
-            c=2
-            for i in range(b):
-                time+=1
-                x=x+dx[c]
-                y=y+dy[c]
-                if x==0 and y==0:
-                    print(time)
-                    return False
-                    
-        if a=="E":
-            c=0
-            for i in range(b):
-                time+=1
-                x=x+dx[c]
-                y=y+dy[c]
-                if x==0 and y==0:
-                    print(time)
-                    break
+a_list=[]
 
-if False:
+class Great():
+    def __init__(self,time,nx,ny):
+        self.nx=nx
+        self.ny=ny
+        self.time=time
+
+for i in range(n):
+    w=input().split()
+    a,b=w[0],int(w[1])
+    if a=="N":
+        c=3
+        for i in range(b):
+            time+=1
+            x=x+dx[c]
+            y=y+dy[c]
+            a_list.append(Great(time,x,y))
+    if a=="S":
+        c=1
+        for i in range(b):
+            time+=1
+            x=x+dx[c]
+            y=y+dy[c]
+            a_list.append(Great(time,x,y))
+    if a=="W":
+        c=2
+        for i in range(b):
+            time+=1
+            x=x+dx[c]
+            y=y+dy[c]
+            a_list.append(Great(time,x,y))
+    if a=="E":
+        c=0
+        for i in range(b):
+            time+=1
+            x=x+dx[c]
+            y=y+dy[c]
+            a_list.append(Great(time,x,y))
+
+found = False
+for Great in a_list:
+    if Great.nx==0 and Great.ny==0:
+        print(Great.time)
+        found= True
+
+if not found:
     print("-1")
