a28741eec3062ca96ebd0ff766b819618963682f
dbfgdbdfg <2464012@ewha.ac.kr>
2024-08-11 15:22:16 +0900

    Title: 이동경로상에 있는 모든 숫자 더하기, Time: 141ms, Memory: 28MB, Status: Passed - CodeTree

--- 240811/이동경로상에 있는 모든 숫자 더하기/add-all-the-numbers-on-the-path.py
+++ 240811/이동경로상에 있는 모든 숫자 더하기/add-all-the-numbers-on-the-path.py
@@ -5,20 +5,21 @@ a_list=[list(map(int,input().split()))
 sums=0
 dx,dy=[-1,0,1,0],[0,1,0,-1]
 dire=0
-x,y=(n-1)//2,(n-2)//2
+x,y=(n-1)//2,(n-1)//2
+
+sums+=a_list[x][y]
 
 for i in range(t):
-    
-
     if tts[i]=="L":
         dire=(dire+3)%4
-    elif tts[i]=="R":
+    if tts[i]=="R":
         dire=(dire+1)%4
-    elif tts[i]=="F":
+    if tts[i]=="F":
         nx=x+dx[dire]
         ny=y+dy[dire]
         if 0<=nx<n and 0<=ny<n:
             sums+=a_list[nx][ny]
+    
             x=nx
             y=ny
         else:
