25aa1477454195a44db09965a405d7991325799e
dbfgdbdfg <2464012@ewha.ac.kr>
2024-08-11 14:48:11 +0900

    Title: 빙빙 돌며 사각형 채우기, Time: 144ms, Memory: 26MB, Status: Wrong Answer - CodeTree

---
+++ 240811/빙빙 돌며 사각형 채우기/snail-alphabet-square.py
@@ -0,0 +1,30 @@
+n,m=map(int,input().split())
+a_list=[[0]*m for i in range(n)]
+
+r,c=0,0
+dx,dy=[1,0,-1,0],[0,1,0,-1]
+
+dire=1
+   
+def Great(a,b,dire):
+    if (0<=a<n and 0<=b<m) and a_list[a][b]==0:
+        return dire
+    else:
+        dire=(dire-1)%4
+        return dire
+
+
+for i in range(1,n*m+1):
+    if 64+i>90:
+        i=i-26
+    a_list[r][c]=chr(64+i)
+    nr=r+dx[dire]
+    nc=c+dy[dire]
+    #print(r,c,i,dire,nr,nc,"d")
+    dire=Great(nr,nc,dire)
+    r=r+dx[dire]
+    c=c+dy[dire]
+for row in a_list:
+    for element in row:
+        print(element,end=" ")
+    print()
