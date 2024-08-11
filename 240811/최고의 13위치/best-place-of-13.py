480ffb1f9b953cefe05ad6a0396f2d815875b79b
dbfgdbdfg <2464012@ewha.ac.kr>
2024-08-11 16:29:37 +0900

    Title: 최고의 13위치, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240811/최고의 13위치/best-place-of-13.py
@@ -0,0 +1,14 @@
+n=int(input())
+
+a_list= [list(map(int,input().split()))
+    for i in range(n)]
+
+sums=0
+b_list=[]
+
+for c in range(n):
+    for r in range(n-2):
+        sums+=a_list[r][c]
+    b_list.append(sums)
+        
+print(max(b_list))
