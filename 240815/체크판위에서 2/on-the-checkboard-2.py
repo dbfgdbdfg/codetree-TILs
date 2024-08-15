19a28bfff9d8c945fe66610f877c694482781919
dbfgdbdfg <2464012@ewha.ac.kr>
2024-08-15 16:33:44 +0900

    Title: 체크판위에서 2, Time: 125ms, Memory: 24MB, Status: Passed - CodeTree

--- 240815/체크판위에서 2/on-the-checkboard-2.py
+++ 240815/체크판위에서 2/on-the-checkboard-2.py
@@ -11,32 +11,40 @@ for i in range(r)]
 
 w_list=[]
 b_list=[]
+if a_list[0][0]==a_list[r-1][c-1]:
+    print("0")
 
-if a_list[0][0]=="W":
+
+
+
+
+
+
+if a_list[0][0]=="W" and a_list[r-1][c-1]=="B":
     for x in range(1,r-1):
         for y in range(1,c-1):
             if a_list[x][y]=="B":
                 b_list.append(Great(x,y))
     for i in range(len(b_list)):
-        for x in range(b_list[i].r+1,r-1):
-            for y in range(b_list[i].c+1,c-1):
-                if a_list[x][y]=="W":
-                    w_list.append(Great(x,y))
+        for q in range(b_list[i].r +1,r-1):
+            for w in range(b_list[i].c +1,c-1):
+                if a_list[q][w]=="W":
+                    w_list.append(Great(q,w))
         
     print(len(w_list))
 
 # W B W B
 
-
-if a_list[0][0]=="B":
-    for x in range(r):
-        for y in range(c):
+if a_list[0][0]=="B" and a_list[r-1][c-1]=="W":
+    for x in range(1,r-1):
+        for y in range(1,c-1):
             if a_list[x][y]=="W":
                 w_list.append(Great(x,y))
     for i in range(len(w_list)):
-        for x in range(w_list[i].r+1,r-1):
-            for y in range(w_list[i].c+1,c-1):
-                if a_list[x][y]=="B":
-                    b_list.append(Great(x,y))
+        for q in range(w_list[i].r +1,r-1):
+            for w in range(w_list[i].c +1,c-1):
+                if a_list[q][w]=="B":
+                    b_list.append(Great(q,w))
+
 
     print(len(b_list))
