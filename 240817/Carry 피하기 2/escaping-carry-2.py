da772d4247d62136289e5269cd24b63b80bbeed6
dbfgdbdfg <2464012@ewha.ac.kr>
2024-08-17 14:05:37 +0900

    Title: Carry 피하기 2, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 240817/Carry 피하기 2/escaping-carry-2.py
+++ 240817/Carry 피하기 2/escaping-carry-2.py
@@ -6,7 +6,7 @@ what_list=[]
 
 
 def Great(x,y,z):
-    while max(x,y,z)>10:
+    while max(x,y,z)>0:
         if x%10 + y%10 + z%10 >=10:
             return False
             break
@@ -19,9 +19,8 @@ def Great(x,y,z):
 for i in range(n-2):
     for q in range(i+1,n-1):
         for w in range(q+1,n):
-            
-
             if Great(a_list[i],a_list[q],a_list[w])==True:
+                #print(a_list[i],a_list[q],a_list[w])
                 sums=a_list[i]+a_list[q]+a_list[w]
                 what_list.append(sums)
                 sums=0
