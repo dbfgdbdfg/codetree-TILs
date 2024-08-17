88a83b4f7e2672355ea8999d6f7e10d60b700887
dbfgdbdfg <2464012@ewha.ac.kr>
2024-08-17 14:38:33 +0900

    Title: 오목, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240817/오목/O-mok.py
+++ 240817/오목/O-mok.py
@@ -26,20 +26,19 @@ for r in range(14):
                 if a_list[r+2][c+2]==a_list[r+3][c+3]==a_list[r+4][c+4]==1:
                     black=True
                     black_list.append(Great(r+2,c+2))
-        if a_list[r][c]==2:
+        elif a_list[r][c]==2:
             if a_list[r+1][c]==2:
                 if a_list[r+2][c]==a_list[r+3][c]==a_list[r+4][c]==2:
                     white=True
                     white_list.append(Great(r+2,c))
             if a_list[r][c+1]==2:
                 if a_list[r][c+2]==a_list[r][c+3]==a_list[r][c+4]==2:
-                    White=True
+                    white=True
                     white_list.append(Great(r,c+2))
             if a_list[r+1][c+1]==2:
                 if a_list[r+2][c+2]==a_list[r+3][c+3]==a_list[r+4][c+4]==2:
-                    White=True
+                    white=True
                     white_list.append(Great(r+2,c+2))
-
 
 
 if black==True:
@@ -47,6 +46,6 @@ if black==True:
     print(black_list[0].r+1,black_list[0].c+1)
 elif white==True:
     print("2")
-    print(White_list[0].r+1,White_list[0].c+1)
+    print(white_list[0].r+1,white_list[0].c+1)
 else:
     print("0")
