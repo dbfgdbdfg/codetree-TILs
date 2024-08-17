8ea7e43456d506da955baa5baaf3f98a111aec03
dbfgdbdfg <2464012@ewha.ac.kr>
2024-08-17 12:33:32 +0900

    Title: 이상한 진수 2, Time: 123ms, Memory: 24MB, Status: Passed - CodeTree

--- 240817/이상한 진수 2/awkward-digits-2.py
+++ 240817/이상한 진수 2/awkward-digits-2.py
@@ -1,14 +1,18 @@
 a_list=list(map(int,input()))
 b_list=[]
+
 num_list=[]
 num=0
 for i in range(len(a_list)):
-    b_list=a_list
+    b_list=a_list.copy()
+
     b_list[i]=(b_list[i]+1)%2
     for i in range(len(a_list)):
         num=num*2 + b_list[i]
     num_list.append(num)
-    num=0
+    num=0 
+
+
 
 
 print(max(num_list))
