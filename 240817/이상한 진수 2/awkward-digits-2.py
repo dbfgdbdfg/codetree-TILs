749ee732f85f79087fdd464cf6af20ff3e4e956d
dbfgdbdfg <2464012@ewha.ac.kr>
2024-08-17 12:27:05 +0900

    Title: 이상한 진수 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240817/이상한 진수 2/awkward-digits-2.py
@@ -0,0 +1,14 @@
+a_list=list(map(int,input()))
+b_list=[]
+num_list=[]
+num=0
+for i in range(1,len(a_list)):
+    b_list=a_list
+    b_list[i]=(b_list[i]+1)%2
+    for i in range(len(a_list)):
+        num=num*2 + b_list[i]
+    num_list.append(num)
+    num=0
+
+
+print(max(num_list))
