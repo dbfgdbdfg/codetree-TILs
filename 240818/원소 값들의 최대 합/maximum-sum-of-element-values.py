363c62e3e59597c9debdc2ad3b9ffe06fe2d03bd
dbfgdbdfg <2464012@ewha.ac.kr>
2024-08-18 17:29:58 +0900

    Title: 원소 값들의 최대 합, Time: 119ms, Memory: 25MB, Status: Passed - CodeTree

--- 240818/원소 값들의 최대 합/maximum-sum-of-element-values.py
+++ 240818/원소 값들의 최대 합/maximum-sum-of-element-values.py
@@ -8,21 +8,33 @@ a_list=[]
 for i in range(n):
     a_list.append(Great(i+1,int(w[i])))
 
-movement=0
-move_total=0
+def what(i, a_list):
+    movement = 0
+    move_total = 0
+    while movement < m:
+        move_total += a_list[i - 1].number
+        movement += 1
+        i = a_list[i - 1].number  # 다음 인덱스를 현재 값으로 설정
+        if i <= 0 or i > n:  # 유효하지 않은 인덱스에 도달하면 종료
+            break
+
+    return move_total
+
+
+'''
+
 
 def what(i,a_list,movement=0,move_total=0):
-
     if movement>=m:
         return move_total
     move_total+=a_list[i-1].number
     movement+=1
     return what(a_list[i-1].number,a_list,movement,move_total)
+    '''
 move_list=[]
 
 for i in range(1,n+1):
     move_list.append(what(i,a_list))
-
 
 print(max(move_list))
     
