0cafce53e47ce910f0f4833519a1f6b9ca6dee0a
dbfgdbdfg <2464012@ewha.ac.kr>
2024-08-18 16:46:12 +0900

    Title: 팰린드롬 수 찾기, Time: 150ms, Memory: 25MB, Status: Passed - CodeTree

--- 240818/팰린드롬 수 찾기/find-the-number-of-palindrome.py
+++ 240818/팰린드롬 수 찾기/find-the-number-of-palindrome.py
@@ -8,17 +8,21 @@ def Great(n):
     what=len(n)//2
     for i in range(what):
         if n[i]==n[-i-1]:
-            return True
+            continue
         else:
             return False
             break
+            
+
+#print(Great(1011))
+
 
 #짝수면 0이랑 -1 이랑 1이랑 -2이랑 같아야 됨
 
 
 
-
 for i in range(x,y+1):
-    if Great(i):
+    if Great(i)!=False:
+        #print(i)
         nums+=1
 print(nums)
