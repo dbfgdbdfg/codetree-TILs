2659b992ffed0d1ccba6e3014eae94cab8c0b8bf
dbfgdbdfg <2464012@ewha.ac.kr>
2024-08-22 14:45:03 +0900

    Title: 전부 겹치는 선분 2, Time: 133ms, Memory: 28MB, Status: Passed - CodeTree

--- 240822/전부 겹치는 선분 2/overlapping-line-segments-2.py
+++ 240822/전부 겹치는 선분 2/overlapping-line-segments-2.py
@@ -17,12 +17,14 @@ for i in range(n):
         for w in range(q+1,n-1):
             if b_list[q].y<b_list[w].x or b_list[w].y<b_list[q].x:
                 what=False
+    #print(i,what)
     if what == True:
         #print(max(b_list[0].x,b_list[1].x))
         for i in range(int(max(b_list[0].x,b_list[1].x)),min(b_list[0].y,b_list[1].y)+1):
             for w in range(2,n-1):
-                if b_list[w].x>i or b_list[w].y<i:
-                    what=False
+                if b_list[w].x<=i and b_list[w].y>=i:
+                    what=True
+                    break
 
                 
     #print(i,what)
