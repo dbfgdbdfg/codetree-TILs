49c220446178fa86d0ca3050cd6256359e115df4
dbfgdbdfg <2464012@ewha.ac.kr>
2024-08-22 16:54:42 +0900

    Title: 최대 최소간의 차, Time: 159ms, Memory: 25MB, Status: Passed - CodeTree

--- 240822/최대 최소간의 차/difference-between-maximum-and-minimum.py
+++ 240822/최대 최소간의 차/difference-between-maximum-and-minimum.py
@@ -2,16 +2,18 @@ n,k=map(int,input().split())
 a_list=list(map(int,input().split()))
 a_list.sort()
 b_list=[]
+
 def Great(first,end,a_list):
+    sums=0
     for i in range(len(a_list)):
         if a_list[i]>=first and a_list[i]<=end:
-            a_list[i]=0
+            sums+=0
         elif a_list[i]<first:
-            a_list[i]=first-a_list[i]
+            sums+=first-a_list[i]
         elif a_list[i]>end:
-            a_list[i]=a_list[i]-end
-    print(a_list)
-    return sum(a_list)
+            sums+=a_list[i]-end
+    #print(a_list)
+    return sums
 #3 20 27 62 
 first=a_list[0]
 end=first+k
@@ -19,7 +21,6 @@ end=first+k
 sums=0
 sum_list=[]
 for i in range(first,a_list[-1]-k+1):
-    sum_list.append(Great(i,i+k,a_list))
-    
+    sum_list.append(Great(i,i+k,a_list))    
 
 print(min(sum_list))
