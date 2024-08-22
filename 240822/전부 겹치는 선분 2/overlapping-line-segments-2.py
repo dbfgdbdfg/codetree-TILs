1411c36f8cf2b826918ba176cbb153048aa0da09
dbfgdbdfg <2464012@ewha.ac.kr>
2024-08-22 14:21:55 +0900

    Title: 전부 겹치는 선분 2, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240822/전부 겹치는 선분 2/overlapping-line-segments-2.py
@@ -0,0 +1,26 @@
+n=int(input())
+class Great:
+    def __init__ (self,x,y):
+        self.x=x
+        self.y=y
+a_list=[]
+for i in range(n):
+    w=input().split()
+    a_list.append(Great(w[0],w[1]))
+b_list=[]
+
+for i in range(n):
+    b_list=a_list.copy()
+    del b_list[i]
+    what=True
+    for q in range(n-2):
+        for w in range(q+1,n-1):
+            if b_list[q].y<b_list[w].x or b_list[w].y<b_list[q].x:
+                what=False
+        
+    if what==True:
+            print("Yes")
+            break
+
+#안겹치시 ㅌ1 ㅛ1  ㅌ2 ㅛ2
+# ㅌ2 ㅛ2 ㅌ1 ㅛ1
